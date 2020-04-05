baa89c2c4701d7c41323152b6d023c771c47c1c8
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2020-04-05 17:02:00 -0400

    bumped up ring-undertow-adapter

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus-undertow "0.1.0"
+(defproject luminus-undertow "0.1.1"
   :description "Undertow adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-undertow"
   :license {:name "Eclipse Public License"
@@ -6,4 +6,4 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.logging "0.5.0"]
                  [org.clojure/core.async "0.3.465"]
-                 [luminus/ring-undertow-adapter "1.0.0"]])
+                 [luminus/ring-undertow-adapter "1.0.3"]])
