ad87455c9b91bac3c5f30bc5e0d6c1e5838fc65c
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2020-05-15 20:43:06 -0400

    removed core.async

--- project.clj
+++ project.clj
@@ -4,6 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.logging "0.5.0"]
-                 [org.clojure/core.async "0.3.465"]
+                 [org.clojure/tools.logging "1.1.0"]
                  [luminus/ring-undertow-adapter "1.0.5"]])
