5060fd1c0478fd48b541b29f9eeffc6f6d7a6227
Nik Peric <nik@nikperic.com>
2021-02-20 07:54:23 -0500

    Bump ring-undertow-adapter, bump clojure

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject luminus-undertow "0.1.8"
+(defproject luminus-undertow "0.1.9"
   :description "Undertow adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-undertow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.2"]
                  [org.clojure/tools.logging "1.1.0"]
-                 [luminus/ring-undertow-adapter "1.1.4"]])
+                 [luminus/ring-undertow-adapter "1.1.5"]])
