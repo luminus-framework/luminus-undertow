e4e3e1c9b1769b09012750b8c3e2ff46f120efbe
Nik Peric <nik@nikperic.com>
2022-10-10 09:15:38 +0200

    dump dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject luminus-undertow "0.1.15"
+(defproject luminus-undertow "0.1.16"
   :description "Undertow adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-undertow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/tools.logging "1.1.0"]
-                 [luminus/ring-undertow-adapter "1.2.7"]])
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [luminus/ring-undertow-adapter "1.2.8"]])
